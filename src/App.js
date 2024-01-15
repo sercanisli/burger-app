670a0a994f016be7d9238793b2d278e2fb4fbc90
Sercan ISLI <sercanislii@gmail.com>
2024-01-16 01:45:20 +0300

    Components created. Links structer created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './components/Home'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' exact element = { <Home /> } />
+        {/* exact ile bunun ilk açılış sayfam olacağını belirtiyorum. */}
+        <Route path='/menu' exact element = { <Menu /> } />
+        <Route path='/about' exact element = { <About /> } />
+        <Route path='/contact' exact element = { <Contact /> } />
+      </Routes>
     </div>
   );
 }
